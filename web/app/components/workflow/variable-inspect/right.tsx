35fff6a1645c5cdd0639c6ebec5cc00f47fc5758
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-05-27 19:42:32 -0400

    spelling: uppercase
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- web/app/components/workflow/variable-inspect/right.tsx
+++ web/app/components/workflow/variable-inspect/right.tsx
@@ -256,7 +256,7 @@ const Right = ({
               {!isTruncated && currentNodeVar.var.edited && (
                 <Badge>
                   <span className="mr-[4.5px] ml-[2.5px] h-[3px] w-[3px] rounded-sm bg-text-accent-secondary"></span>
-                  <span className="system-2xs-semibold-uupercase">{t('debug.variableInspect.edited', { ns: 'workflow' })}</span>
+                  <span className="system-2xs-semibold-uppercase">{t('debug.variableInspect.edited', { ns: 'workflow' })}</span>
                 </Badge>
               )}
               {!isTruncated && currentNodeVar.var.edited && currentNodeVar.var.type !== VarInInspectType.conversation && (
